bcd2fba6f70cda13a295be2e2df93000543da389
Vivek Devre <vivekdevre45@gmail.com>
2022-01-26 10:05:10 +0530

    Initial commit.

---
+++ app/src/main/java/com/example/socialmediaapp/daos/PostDao.kt
@@ -0,0 +1,26 @@
+package com.example.socialmediaapp.daos
+
+import com.example.socialmediaapp.models.Post
+import com.example.socialmediaapp.models.User
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+
+class PostDao {
+    val db = FirebaseFirestore.getInstance()
+    val postCollections = db.collection("posts")
+    val auth = Firebase.auth
+    fun addPost(text:String){
+        val currentUserId = auth.currentUser!!.uid
+        GlobalScope.launch {
+            val userDao = UserDao()
+            val user = userDao.getUserById(currentUserId).await().toObject(User::class.java)!!
+            val currentTime = System.currentTimeMillis()
+            val post = Post(text,user,currentTime)
+            postCollections.document().set(post)
+        }
+    }
+}
