4da6c413162bb25cf6387042d23f1e60786b4984
Vivek Devre <vivekdevre45@gmail.com>
2022-01-31 16:45:13 +0530

    Fourth commit.

--- app/src/main/java/com/example/socialmediaapp/MainActivity.kt
+++ app/src/main/java/com/example/socialmediaapp/MainActivity.kt
@@ -3,11 +3,16 @@ package com.example.socialmediaapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.socialmediaapp.daos.PostDao
 import com.example.socialmediaapp.models.Post
 import com.firebase.ui.firestore.FirestoreRecyclerOptions
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.Query
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_main.*
 
 
@@ -15,14 +20,15 @@ class MainActivity : AppCompatActivity(), IPostAdapter {
 
     private lateinit var postDao: PostDao
     private lateinit var adapter: PostAdapter
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         fab.setOnClickListener{
             val intent = Intent(this, CreatePostActivity::class.java)
             startActivity(intent)
-
         }
+        auth= Firebase.auth
         setUpRecyclerView()
     }
     private fun setUpRecyclerView(){
@@ -49,5 +55,20 @@ class MainActivity : AppCompatActivity(), IPostAdapter {
 
     override fun onLikeClicked(postId: String) {
         postDao.updateLikes(postId)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId==R.id.miSignout){
+            auth.signOut()
+            val signOutIntent = Intent(this,SignInActivity::class.java)
+            signOutIntent.flags=Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(signOutIntent)
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
